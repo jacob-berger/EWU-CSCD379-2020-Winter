53e62f0a010260a5c8cb81abfcee28f612f47eff
Jacob Berger <jberger.1919@gmail.com>
2020-01-12 20:53:18 -0800

    Initial commit
    Tests and assignment done.

---
+++ SecretSanta/src/SecretSanta.Business/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    public class User
+    {
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Gift> Gifts { get; }
+
+        public User(int id, string first, string last, List<Gift> gifts)
+        {
+            Id = id;
+            FirstName = first ?? throw new ArgumentNullException(nameof(first));
+            LastName = last ?? throw new ArgumentNullException(nameof(last));
+            Gifts = gifts ?? new List<Gift>();
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.BusinessTests/GiftTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business.Tests
+{
+    [TestClass()]
+    public class GiftTests
+    {
+        [TestMethod()]
+        public void NewGift_GoodValues_Success()
+        {
+            //Arrange
+            int id = 10;
+            string title = "Title";
+            string description = "Description";
+            string url = "URL";
+            User user = new User(1, "First", "Last", new List<Gift>());
+
+            //Act
+            Gift gift = new Gift(id, title, description, url, user);
+
+            //Assert
+            Assert.AreEqual(gift.Id, id);
+            Assert.AreEqual(gift.Title, title);
+            Assert.AreEqual(gift.Description, description);
+            Assert.AreEqual(gift.Url, url);
+            Assert.IsNotNull(gift.User);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NewGift_NullTitle_ThrowsException()
+        {
+            //Arrange
+            //Act
+            Gift gift = new Gift(1, null!, "Description", "URL", new User(2, "First", "Last", new List<Gift>()));
+            
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NewGift_NullDescription_ThrowsException()
+        {
+            //Arrange
+            //Act
+            Gift gift = new Gift(1, "Title", null!, "URL", new User(2, "First", "Last", new List<Gift>()));
+
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NewGift_NullUrl_ThrowsException()
+        {
+            //Arrange
+            //Act
+            Gift gift = new Gift(1, "Title", "Description", null!, new User(2, "First", "Last", new List<Gift>()));
+
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NewGift_NullUser_Success()
+        {
+            //Arrange
+            //Act
+            Gift gift = new Gift(1, "Title", "Description", "URL", null!);
+        }
+    }
+}
