4db915b7d2181bca3a02d6c15f6ca9d294058987
Jacob Berger <jberger.1919@gmail.com>
2020-01-14 21:38:28 -0800

    Added missing test classes

---
+++ SecretSanta/src/SecretSanta.ApiTests/StartupTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Api.Tests
+{
+    [TestClass()]
+    public class StartupTests
+    {
+        [TestMethod()]
+        public void ConfigureTest()
+        {
+
+        }
+    }
+}
